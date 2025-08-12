44978393009bcfd3f57072b869e119bbc4bf5e17
NdNkosi <ndumiso95nkosi@gmail.com>
2025-08-12 13:59:54 +0200

    Delete iris_nn.py

--- iris_nn.py
+++
@@ -1,6 +0,0 @@
-import sklearn
-from sklearn.preprocessing import OneHotEncoder
-
-print("scikit-learn version:", sklearn.__version__)
-
-encoder = OneHotEncoder(sparse=False)  # or sparse_output=False if version >= 1.2
